2f5df13d3fdf0eb831efe11f89400111fd3f6e47
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-11-04 18:57:51 +0000

    change title and my_dict

--- my_project/myfirst_app/views.py
+++ my_project/myfirst_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_me' : "now i am learning something"}
+    my_dict = {'insert_content' : "Hello from myfirst_app"}
     return render(request, "myfirst_app/index.html", context=my_dict)
