3efaf66e3834ae5489ee8024d9d32a46e972fbf4
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-11-06 20:55:32 +0000

    got data from db and put the data in table

--- my_project/myfirst_app/views.py
+++ my_project/myfirst_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myfirst_app.models import Topic, AccessRecord, Webpage
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_content' : "Hello from myfirst_app"}
-    return render(request, "myfirst_app/index.html", context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+
+    date_dict = {"access_records": webpages_list}
+
+    
+    return render(request, "myfirst_app/index.html", context=date_dict)
